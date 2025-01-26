475ea52bd83153a43e4ba1b4c7d79ed923e075e4
Joel-Allan-NZ <joelcallan@gmail.com>
2025-01-26 23:51:42 +1300

    added 2024 d22

--- solutions/2024/day21.ts
+++ solutions/2024/day21.ts
@@ -1,55 +1,174 @@
 export function partOne(input: string[]): number | string {
-  const towels = input[0].split(/, /)
-  const patterns = input.slice(2)
-
-  return patterns.reduce(
-    (total, pattern) => (canMakePattern(pattern, towels) ? total + 1 : total),
-    0
-  )
+  return findComplexity(2, input)
 }
 
-function canMakePattern(pattern: string, towels: string[]): boolean {
-  const patterns = []
-  patterns.push(pattern)
-  while (patterns.length > 0) {
-    let currentPattern = patterns.pop()
+interface Key {
+  x: number
+  y: number
+  c: string
+}
 
-    for (let towel of towels) {
-      if (currentPattern?.startsWith(towel)) {
-        if (currentPattern.length == towel.length) return true
-
-        patterns.push(currentPattern.slice(towel.length))
-      }
-    }
+class RobotCache {
+  cache: Map<string, Map<string, Map<number, number>>>
+  constructor() {
+    this.cache = new Map<string, Map<string, Map<number, number>>>()
   }
-  return false
+
+  setCache(a: string, b: string, robots: number, count: number) {
+    if (!this.cache.has(a))
+      this.cache.set(a, new Map<string, Map<number, number>>())
+    if (!this.cache.get(a)?.has(b))
+      this.cache.get(a)?.set(b, new Map<number, number>())
+
+    this.cache.get(a)?.get(b)?.set(robots, count)
+  }
+
+  tryGetCache(a: string, b: string, robots: number) {
+    return this.cache.get(a)?.get(b)?.get(robots)
+  }
+}
+
+function findComplexity(robotCount: number, input: string[]): number {
+  const { numPad, directionalPad } = generateKeyDictionaries()
+  const cache = new RobotCache()
+  let score = 0
+
+  for (let line of input) {
+    const moves = numPadMoves(line, 'A', numPad)
+
+    const minCost = moves.reduce((minimum, current) => {
+      const pairs = current
+        .split('')
+        .map((item, index) => [index == 0 ? 'A' : current[index - 1], item])
+      const pairsCost = pairs.reduce((total, pair) => {
+        return (
+          total + findMinimumStepCost(pair, robotCount, cache, directionalPad)
+        )
+      }, 0)
+      return pairsCost < minimum ? pairsCost : minimum
+    }, Number.POSITIVE_INFINITY)
+
+    score += minCost * parseInt(line.slice(0, line.length - 1))
+  }
+  return score
+}
+
+function generateKeyDictionaries(): {
+  numPad: Map<string, Key>
+  directionalPad: Map<string, Key>
+} {
+  const numKeys = ['789', '456', '123', ' 0A']
+  const dirKeys = [' ^A', '<v>']
+  const directionalPad = new Map<string, Key>()
+  const numPad = new Map<string, Key>()
+  dirKeys
+    .flatMap((row, y) => row.split('').map((c, x) => ({ x, y, c })))
+    .forEach((key) => {
+      if (key.c != ' ') directionalPad.set(key.c, key)
+    })
+  numKeys
+    .flatMap((row, y) => row.split('').map((c, x) => ({ x, y, c })))
+    .forEach((key) => {
+      if (key.c != ' ') numPad.set(key.c, key)
+    })
+  return { numPad, directionalPad }
+}
+
+function numPadMoves(
+  code: string,
+  start: string,
+  keys: Map<string, Key>
+): string[] {
+  const result: string[] = []
+  const end = code[0]
+  const moves = generateMoves(keys.get(start)!, keys.get(end)!)
+
+  moves.forEach((move) => {
+    if (code.length == 1) moves.forEach((move) => result.push(move + 'A'))
+    else
+      moves
+        .flatMap((move) =>
+          numPadMoves(code.slice(1), end, keys).map(
+            (partialPath) => move + 'A' + partialPath
+          )
+        )
+        .forEach((move) => result.push(move))
+  })
+  return result
+}
+
+function generateMoves(start: Key, end: Key): string[] {
+  const result: string[] = []
+  const stack: { x: number; y: number; path: string[] }[] = []
+  stack.push({ x: start.x, y: start.y, path: [] })
+
+  while (stack.length > 0) {
+    const { x, y, path } = stack.shift()!
+    if (x == end.x && y == end.y) {
+      result.push(path.join(''))
+      continue
+    }
+    const potentialNumber = parseInt(end.c)
+    if (x == 0 && y == 3 && !isNaN(potentialNumber)) continue
+    if (x == 0 && y == 0 && isNaN(potentialNumber)) continue
+
+    if (x != end.x)
+      stack.push(
+        x < end.x
+          ? { x: x + 1, y, path: [...path, '>'] }
+          : { x: x - 1, y, path: [...path, '<'] }
+      )
+    if (y != end.y)
+      stack.push(
+        y < end.y
+          ? { x, y: y + 1, path: [...path, 'v'] }
+          : { x, y: y - 1, path: [...path, '^'] }
+      )
+  }
+  return result
+}
+
+function findMinimumStepCost(
+  step: string[],
+  robotCount: number,
+  cache: RobotCache,
+  directionalPad: Map<string, Key>
+): number {
+  let cachedValue = cache.tryGetCache(step[0], step[1], robotCount)
+  if (!cachedValue) {
+    const moves = generateMoves(
+      directionalPad.get(step[0])!,
+      directionalPad.get(step[1])!
+    )
+
+    cachedValue = moves.reduce((min, move) => {
+      const paths = (move + 'A')
+        .split('')
+        .map((c, i) => [i == 0 ? 'A' : move[i - 1], c])
+
+      const cost = pathCost(paths, robotCount, cache, directionalPad)
+
+      return cost > min ? min : cost
+    }, Number.POSITIVE_INFINITY)
+
+    cache.setCache(step[0], step[1], robotCount, cachedValue)
+  }
+  return cachedValue
+}
+
+function pathCost(
+  paths: string[][],
+  robotCount: number,
+  cache: RobotCache,
+  directionalPad: Map<string, Key>
+): number {
+  return paths.reduce((total, path) => {
+    return robotCount == 1
+      ? total + 1
+      : total + findMinimumStepCost(path, robotCount - 1, cache, directionalPad)
+  }, 0)
 }
 
 export function partTwo(input: string[]): number | string {
-  const towels = input[0].split(/, /)
-  const patterns = input.slice(2)
-  const cache = new Map<string, number>()
-
-  return patterns.reduce(
-    (total, pattern) => total + countValidPatterns(cache, pattern, towels),
-    0
-  )
-}
-
-function countValidPatterns(
-  cache: Map<string, number>,
-  pattern: string,
-  towels: string[]
-): number {
-  if (!pattern || pattern.length == 0) return 1
-  if (cache.has(pattern)) return cache.get(pattern)!
-
-  const prefixMatches = towels.filter((towel) => pattern.startsWith(towel))
-  const matchCount = prefixMatches.reduce(
-    (total, towel) =>
-      total + countValidPatterns(cache, pattern.slice(towel.length), towels),
-    0
-  )
-  cache.set(pattern, matchCount)
-  return matchCount
+  return findComplexity(25, input)
 }
--- solutions/2024/day22.ts
+++ solutions/2024/day22.ts
@@ -1,55 +1,75 @@
 export function partOne(input: string[]): number | string {
-  const towels = input[0].split(/, /)
-  const patterns = input.slice(2)
+  const secrets = input.map((x) => parseInt(x))
 
-  return patterns.reduce(
-    (total, pattern) => (canMakePattern(pattern, towels) ? total + 1 : total),
-    0
-  )
+  return secrets.reduce((total, secret) => {
+    for (let i = 0; i < 2000; i++) secret = getNextSecret(secret)
+
+    return total + secret
+  }, 0)
 }
 
-function canMakePattern(pattern: string, towels: string[]): boolean {
-  const patterns = []
-  patterns.push(pattern)
-  while (patterns.length > 0) {
-    let currentPattern = patterns.pop()
+function getNextSecret(secret: number): number {
+  secret ^= secret << 6
+  secret = ((secret % 16777216) + 16777216) % 16777216
 
-    for (let towel of towels) {
-      if (currentPattern?.startsWith(towel)) {
-        if (currentPattern.length == towel.length) return true
+  secret ^= secret >> 5
+  secret = ((secret % 16777216) + 16777216) % 16777216
 
-        patterns.push(currentPattern.slice(towel.length))
-      }
-    }
-  }
-  return false
+  secret ^= secret << 11
+  return ((secret % 16777216) + 16777216) % 16777216
 }
 
 export function partTwo(input: string[]): number | string {
-  const towels = input[0].split(/, /)
-  const patterns = input.slice(2)
-  const cache = new Map<string, number>()
-
-  return patterns.reduce(
-    (total, pattern) => total + countValidPatterns(cache, pattern, towels),
-    0
+  const secrets = input.map((x) => parseInt(x))
+  const permTotals: number[][][][] = []
+  for (let i = 0; i < 19; i++) {
+    permTotals.push([])
+    for (let j = 0; j < 19; j++) {
+      permTotals[i].push([])
+      for (let k = 0; k < 19; k++) permTotals[i][j].push(Array(19).fill(0))
+    }
+  }
+  let max = 0
+  secrets.forEach(
+    (secret) => (max = countSecretTotals(secret, permTotals, max))
   )
+  return max
 }
 
-function countValidPatterns(
-  cache: Map<string, number>,
-  pattern: string,
-  towels: string[]
-): number {
-  if (!pattern || pattern.length == 0) return 1
-  if (cache.has(pattern)) return cache.get(pattern)!
+function countSecretTotals(
+  secret: number,
+  permTotals: number[][][][],
+  max: number
+) {
+  const seen = new Set<number>()
+  const secretValues: number[] = [secret % 10]
 
-  const prefixMatches = towels.filter((towel) => pattern.startsWith(towel))
-  const matchCount = prefixMatches.reduce(
-    (total, towel) =>
-      total + countValidPatterns(cache, pattern.slice(towel.length), towels),
-    0
-  )
-  cache.set(pattern, matchCount)
-  return matchCount
+  for (let i = 0; i < 2000; i++) {
+    secret = getNextSecret(secret)
+    secretValues.push(secret % 10)
+
+    if (i > 2) {
+      const lastFour = [
+        9 + secretValues[i + 1] - secretValues[i],
+        9 + secretValues[i] - secretValues[i - 1],
+        9 + secretValues[i - 1] - secretValues[i - 2],
+        9 + secretValues[i - 2] - secretValues[i - 3],
+      ]
+      const s =
+        lastFour[0] * 1000000 +
+        lastFour[1] * 10000 +
+        lastFour[2] * 100 +
+        lastFour[3]
+
+      if (!seen.has(s)) {
+        const res =
+          permTotals[lastFour[0]][lastFour[1]][lastFour[2]][lastFour[3]] +
+          (secret % 10)
+        permTotals[lastFour[0]][lastFour[1]][lastFour[2]][lastFour[3]] = res
+        if (res > max) max = res
+        seen.add(s)
+      }
+    }
+  }
+  return max
 }
