d9c2ef9c955694f626410b2f2943d6a894b53a47
Joel-Allan-NZ <joelcallan@gmail.com>
2025-02-05 17:13:44 +1300

    Feat: added 2023 d11

--- solutions/2023/day11.ts
+++ solutions/2023/day11.ts
@@ -1,63 +1,78 @@
 export function partOne(input: string[]): number | string {
-  const cache = new Map<number, Map<number, number>>()
+  let distance = 0
+  const galaxies = expandGalaxies(parse(input), input, 1)
 
-  return input[0]
-    .split(' ')
-    .map((x) => parseInt(x))
-    .reduce((total, current) => total + applyRules(current, 25, cache), 0)
+  galaxies.forEach((i, index) => {
+    distance += galaxies
+      .slice(index + 1)
+      .reduce(
+        (total, current) =>
+          total + Math.abs(current[0] - i[0]) + Math.abs(current[1] - i[1]),
+        0
+      )
+  })
+
+  return distance
 }
 
-export function applyRules(
-  stone: number,
-  count: number,
-  cache: Map<number, Map<number, number>>
-): number {
-  if (cache.has(stone)) {
-    const blinks = cache.get(stone)
-    if (blinks?.has(count)) return blinks.get(count)!
-  }
+function parse(input: string[]): Map<number, number[]> {
+  const galaxies = new Map<number, number[]>()
 
-  if (count == 0) return nestedAdd(cache, stone, 0, 1)
-  if (stone == 0)
-    return nestedAdd(cache, stone, count, applyRules(1, count - 1, cache))
-  if (Math.floor(Math.log10(stone)) % 2 == 0)
-    return nestedAdd(
-      cache,
-      stone,
-      count,
-      applyRules(stone * 2024, count - 1, cache)
-    )
-
-  let s = stone.toString()
-  let half = Math.floor(s.length / 2)
-  return nestedAdd(
-    cache,
-    stone,
-    count,
-    applyRules(parseInt(s.slice(0, half)), count - 1, cache) +
-      applyRules(parseInt(s.slice(half)), count - 1, cache)
-  )
+  input.forEach((line, y) => {
+    line.split('').forEach((c, x) => {
+      if (c == '#') {
+        if (galaxies.has(y)) galaxies.get(y)!.push(x)
+        else galaxies.set(y, [x])
+      }
+    })
+  })
+  return galaxies
 }
 
-function nestedAdd<T, U, V>(
-  map: Map<T, Map<U, V>>,
-  key: T,
-  nestedKey: U,
-  value: V
-): V {
-  if (!map.has(key)) {
-    map.set(key, new Map<U, V>([[nestedKey, value]]))
-  } else if (!map.get(key)?.has(nestedKey)) {
-    map.get(key)?.set(nestedKey, value)
-  }
-  return value
+function expandGalaxies(
+  galaxies: Map<number, number[]>,
+  input: string[],
+  expandSize: number
+): number[][] {
+  var missingY: number[] = []
+  var missingX: number[] = []
+
+  for (let y = 0; y < input.length; y++) if (!galaxies.has(y)) missingY.push(y)
+
+  for (let x = 0; x < input[0].length; x++)
+    if (!galaxies.values().some((z) => z.includes(x))) missingX.push(x)
+
+  missingY.sort((x, y) => x - y)
+  missingX.sort((x, y) => x - y)
+
+  const effectiveCoords: number[][] = []
+
+  galaxies.keys().forEach((y) => {
+    let yIndex = missingY.findIndex((missing) => missing > y)
+    yIndex = yIndex == -1 ? missingY.length : yIndex
+    const realY = y + expandSize * yIndex
+    galaxies.get(y)!.forEach((x) => {
+      let xIndex = missingX.findIndex((missing) => missing > x)
+      xIndex = xIndex == -1 ? missingX.length : xIndex
+      effectiveCoords.push([realY, x + expandSize * xIndex])
+    })
+  })
+  return effectiveCoords
 }
 
 export function partTwo(input: string[]): number | string {
-  const cache = new Map<number, Map<number, number>>()
+  let distance = 0
+  const galaxies = expandGalaxies(parse(input), input, 999999)
 
-  return input[0]
-    .split(' ')
-    .map((x) => parseInt(x))
-    .reduce((total, current) => total + applyRules(current, 75, cache), 0)
+  galaxies.forEach((i, index) => {
+    distance += galaxies
+      .slice(index + 1)
+      .reduce(
+        (total, current) =>
+          total + Math.abs(current[0] - i[0]) + Math.abs(current[1] - i[1]),
+        0
+      )
+  })
+
+  return distance
 }
