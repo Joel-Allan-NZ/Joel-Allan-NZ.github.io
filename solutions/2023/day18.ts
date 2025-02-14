5631db143fc7c89bbc63d080fa236d4595cfca4f
Joel-Allan-NZ <joelcallan@gmail.com>
2025-02-14 13:08:38 +1300

    feat: added 2023 d18

--- solutions/2023/day18.ts
+++ solutions/2023/day18.ts
@@ -1,138 +1,66 @@
 export function partOne(input: string[]): number | string {
-  const graph = parse(input)
-  const start = graph.get(0)?.get(0)!
-  const end = graph.get(70)?.get(70)!
-  return findShortestExitDistance(graph, start, end)
+  const corners = digTrenches(input)
+
+  return gaussArea(corners) + calculatePerimeter(corners) / 2 + 1
 }
 
-interface CoordAwareNode {
-  X: number
-  Y: number
-  Value: string
-  Edges: CoordAwareNode[]
+function digTrenches(input: string[]): number[][] {
+  let x = 0,
+    y = 0
+  const corners: number[][] = []
+
+  for (let line of input) {
+    const split = line.split(' ')
+    const distance = parseInt(split[1])
+    corners.push([x, y])
+
+    x = split[0] == 'R' ? x + distance : split[0] == 'L' ? x - distance : x
+    y = split[0] == 'D' ? y + distance : split[0] == 'U' ? y - distance : y
+  }
+  return corners
 }
 
-function parse(input: string[]): Map<number, Map<number, CoordAwareNode>> {
-  const graph = new Map<number, Map<number, CoordAwareNode>>()
+function gaussArea(corners: number[][]): number {
+  const total = corners
+    .map((corner, index) =>
+      index == corners.length - 1
+        ? [corner, corners[0]]
+        : [corner, corners[index + 1]]
+    )
+    .reduce(
+      (total, pair) =>
+        total + pair[0][0] * pair[1][1] - pair[1][0] * pair[0][1],
+      0
+    )
 
-  for (let y = 0; y < 71; y++) {
-    for (let x = 0; x < 71; x++) {
-      const node: CoordAwareNode = { X: x, Y: y, Value: '.', Edges: [] }
-      if (!graph.has(y)) graph.set(y, new Map<number, CoordAwareNode>())
-      graph.get(y)?.set(x, node)
-      const neighbours = [graph.get(y - 1)?.get(x), graph.get(y)?.get(x - 1)]
-      neighbours.forEach((neighbour) => {
-        if (neighbour) {
-          neighbour.Edges.push(node)
-          node.Edges.push(neighbour)
-        }
-      })
-    }
-  }
-  for (let i = 0; i < 1024; i++) {
-    let coords = input[i].split(/,/).map((x) => parseInt(x))
-    graph.get(coords[1])!.get(coords[0])!.Value = '#'
-  }
-  return graph
+  return 0.5 * Math.abs(total)
 }
 
-function findShortestExitDistance(
-  graph: Map<number, Map<number, CoordAwareNode>>,
-  start: CoordAwareNode,
-  end: CoordAwareNode
-): number {
-  const minimumDistances = new Map<CoordAwareNode, number>([
-    [end, 2 ** 31],
-    [start, 0],
-  ])
-  const toExplore = [start]
-
-  while (toExplore.length > 0) {
-    const node = toExplore.shift()!
-    const distance = minimumDistances.get(node)!
-
-    if (node == end) return distance
-
-    const possibleMin = minimumDistances.get(node)
-    if (possibleMin && possibleMin < distance) continue
-
-    node.Edges.forEach((node) => {
-      if (node.Value != '#') {
-        const nodeDistance = minimumDistances.get(node)
-        if (!nodeDistance || nodeDistance > distance + 1) {
-          minimumDistances.set(node, distance + 1)
-          toExplore.push(node)
-        }
-      }
-    })
-  }
-  return minimumDistances.get(end)!
+function calculatePerimeter(corners: number[][]) {
+  return corners
+    .map((corner, index) =>
+      index == corners.length - 1
+        ? [corner, corners[0]]
+        : [corner, corners[index + 1]]
+    )
+    .reduce(
+      (total, pair) =>
+        total +
+        Math.abs(pair[0][0] - pair[1][0]) +
+        Math.abs(pair[0][1] - pair[1][1]),
+      0
+    )
 }
 
 export function partTwo(input: string[]): number | string {
-  const graph = parse(input)
-  const start = graph.get(0)?.get(0)!
-  const end = graph.get(70)?.get(70)!
-  const visitedNodes = new Set<CoordAwareNode>()
+  var directions = ['R', 'D', 'L', 'U']
+  const parsedInput = input.map((line) => {
+    var split = line.split(' ')
+    var direction = directions[parseInt(split[2][split[2].length - 2])]
+    return `${direction} ${parseInt(split[2].slice(2, -2), 16)}`
+  })
 
-  for (let i = 1024; i < input.length; i++) {
-    let coords = input[i].split(/,/).map((x) => parseInt(x))
-    graph.get(coords[1])!.get(coords[0])!.Value! = '#'
+  const corners = digTrenches(parsedInput)
 
-    if (
-      visitedNodes.size == 0 ||
-      visitedNodes.has(graph.get(coords[1])!.get(coords[0])!)
-    ) {
-      if (!hasPossiblePath(graph, start, end, visitedNodes))
-        return `${coords[0]},${coords[1]}`
-    }
-  }
-  return 'Can still find a path.'
-}
-
-function hasPossiblePath(
-  graph: Map<number, Map<number, CoordAwareNode>>,
-  start: CoordAwareNode,
-  end: CoordAwareNode,
-  visited: Set<CoordAwareNode>
-): boolean {
-  const priority = new Map<number, CoordAwareNode[]>()
-  visited.clear()
-  visited.add(start)
-  addPriorityNode(priority, start)
-  while (priority.size > 0) {
-    let current = getPriorityNode(priority)
-    if (current == end) return true
-
-    current.Edges.forEach((neighbour) => {
-      if (neighbour.Value != '#' && !visited.has(neighbour)) {
-        visited.add(neighbour)
-        addPriorityNode(priority, neighbour)
-      }
-    })
-  }
-  return false
-}
-
-function addPriorityNode(
-  priority: Map<number, CoordAwareNode[]>,
-  node: CoordAwareNode
-) {
-  const distance = 140 - node.X - node.Y
-
-  if (!priority.has(distance)) priority.set(distance, [node])
-  else priority.get(distance)?.push(node)
-}
-
-function getPriorityNode(
-  priority: Map<number, CoordAwareNode[]>
-): CoordAwareNode {
-  const smallest = priority
-    .keys()
-    .reduce((min, current) => (min > current ? current : min), 2 ** 31)
-  const toReturn = priority.get(smallest)!.shift()!
-  if (priority.get(smallest)?.length == 0) {
-    priority.delete(smallest)
-  }
-  return toReturn
+  return gaussArea(corners) + calculatePerimeter(corners) / 2 + 1
 }
