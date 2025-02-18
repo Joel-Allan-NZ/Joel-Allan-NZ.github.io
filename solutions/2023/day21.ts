62126a76f9a25a8fe9224c65c147eec3776247e1
Joel-Allan-NZ <joelcallan@gmail.com>
2025-02-18 17:56:26 +1300

    feat: added 2023 d21

--- solutions/2023/day21.ts
+++ solutions/2023/day21.ts
@@ -1,174 +1,102 @@
 export function partOne(input: string[]): number | string {
-  return findComplexity(2, input)
+  const distances = findAccessiblePlots(
+    Math.floor(input[0].length / 2),
+    Math.floor(input.length / 2),
+    input,
+    64
+  )
+  return distances
+    .values()
+    .filter((x) => x % 2 == 0)
+    .toArray().length
 }
 
-interface Key {
-  x: number
-  y: number
-  c: string
-}
+function findAccessiblePlots(
+  startX: number,
+  startY: number,
+  input: string[],
+  stepTarget: number
+): Map<string, number> {
+  const distances = new Map<string, number>()
+  distances.set(startX + ',' + startY, 0)
+  const positions = [[startX, startY]]
+  const steps = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ]
 
-class RobotCache {
-  cache: Map<string, Map<string, Map<number, number>>>
-  constructor() {
-    this.cache = new Map<string, Map<string, Map<number, number>>>()
-  }
+  let pos = 0
+  while (positions.length > 0) {
+    let current = positions.shift()!
+    pos++
+    console.log(pos)
+    const currentDistance = distances.get(current.join(','))!
+    if (currentDistance >= stepTarget) continue
 
-  setCache(a: string, b: string, robots: number, count: number) {
-    if (!this.cache.has(a))
-      this.cache.set(a, new Map<string, Map<number, number>>())
-    if (!this.cache.get(a)?.has(b))
-      this.cache.get(a)?.set(b, new Map<number, number>())
+    for (let step of steps) {
+      const next = [current[0] + step[0], current[1] + step[1]]
+      if (next.some((x) => x < 0 || x >= input.length)) continue
 
-    this.cache.get(a)?.get(b)?.set(robots, count)
-  }
-
-  tryGetCache(a: string, b: string, robots: number) {
-    return this.cache.get(a)?.get(b)?.get(robots)
-  }
-}
-
-function findComplexity(robotCount: number, input: string[]): number {
-  const { numPad, directionalPad } = generateKeyDictionaries()
-  const cache = new RobotCache()
-  let score = 0
-
-  for (let line of input) {
-    const moves = numPadMoves(line, 'A', numPad)
-
-    const minCost = moves.reduce((minimum, current) => {
-      const pairs = current
-        .split('')
-        .map((item, index) => [index == 0 ? 'A' : current[index - 1], item])
-      const pairsCost = pairs.reduce((total, pair) => {
-        return (
-          total + findMinimumStepCost(pair, robotCount, cache, directionalPad)
-        )
-      }, 0)
-      return pairsCost < minimum ? pairsCost : minimum
-    }, Number.POSITIVE_INFINITY)
-
-    score += minCost * parseInt(line.slice(0, line.length - 1))
-  }
-  return score
-}
-
-function generateKeyDictionaries(): {
-  numPad: Map<string, Key>
-  directionalPad: Map<string, Key>
-} {
-  const numKeys = ['789', '456', '123', ' 0A']
-  const dirKeys = [' ^A', '<v>']
-  const directionalPad = new Map<string, Key>()
-  const numPad = new Map<string, Key>()
-  dirKeys
-    .flatMap((row, y) => row.split('').map((c, x) => ({ x, y, c })))
-    .forEach((key) => {
-      if (key.c != ' ') directionalPad.set(key.c, key)
-    })
-  numKeys
-    .flatMap((row, y) => row.split('').map((c, x) => ({ x, y, c })))
-    .forEach((key) => {
-      if (key.c != ' ') numPad.set(key.c, key)
-    })
-  return { numPad, directionalPad }
-}
-
-function numPadMoves(
-  code: string,
-  start: string,
-  keys: Map<string, Key>
-): string[] {
-  const result: string[] = []
-  const end = code[0]
-  const moves = generateMoves(keys.get(start)!, keys.get(end)!)
-
-  moves.forEach((move) => {
-    if (code.length == 1) moves.forEach((move) => result.push(move + 'A'))
-    else
-      moves
-        .flatMap((move) =>
-          numPadMoves(code.slice(1), end, keys).map(
-            (partialPath) => move + 'A' + partialPath
-          )
-        )
-        .forEach((move) => result.push(move))
-  })
-  return result
-}
-
-function generateMoves(start: Key, end: Key): string[] {
-  const result: string[] = []
-  const stack: { x: number; y: number; path: string[] }[] = []
-  stack.push({ x: start.x, y: start.y, path: [] })
-
-  while (stack.length > 0) {
-    const { x, y, path } = stack.shift()!
-    if (x == end.x && y == end.y) {
-      result.push(path.join(''))
-      continue
+      if (input[next[1]][next[0]] != '#') {
+        const nextString = next.join(',')
+        const existing = distances.get(nextString)
+        if (!existing || existing > currentDistance + 1) {
+          distances.set(nextString, currentDistance + 1)
+          positions.push(next)
+        }
+      }
     }
-    const potentialNumber = parseInt(end.c)
-    if (x == 0 && y == 3 && !isNaN(potentialNumber)) continue
-    if (x == 0 && y == 0 && isNaN(potentialNumber)) continue
-
-    if (x != end.x)
-      stack.push(
-        x < end.x
-          ? { x: x + 1, y, path: [...path, '>'] }
-          : { x: x - 1, y, path: [...path, '<'] }
-      )
-    if (y != end.y)
-      stack.push(
-        y < end.y
-          ? { x, y: y + 1, path: [...path, 'v'] }
-          : { x, y: y - 1, path: [...path, '^'] }
-      )
   }
-  return result
-}
 
-function findMinimumStepCost(
-  step: string[],
-  robotCount: number,
-  cache: RobotCache,
-  directionalPad: Map<string, Key>
-): number {
-  let cachedValue = cache.tryGetCache(step[0], step[1], robotCount)
-  if (!cachedValue) {
-    const moves = generateMoves(
-      directionalPad.get(step[0])!,
-      directionalPad.get(step[1])!
-    )
-
-    cachedValue = moves.reduce((min, move) => {
-      const paths = (move + 'A')
-        .split('')
-        .map((c, i) => [i == 0 ? 'A' : move[i - 1], c])
-
-      const cost = pathCost(paths, robotCount, cache, directionalPad)
-
-      return cost > min ? min : cost
-    }, Number.POSITIVE_INFINITY)
-
-    cache.setCache(step[0], step[1], robotCount, cachedValue)
-  }
-  return cachedValue
-}
-
-function pathCost(
-  paths: string[][],
-  robotCount: number,
-  cache: RobotCache,
-  directionalPad: Map<string, Key>
-): number {
-  return paths.reduce((total, path) => {
-    return robotCount == 1
-      ? total + 1
-      : total + findMinimumStepCost(path, robotCount - 1, cache, directionalPad)
-  }, 0)
+  return distances
 }
 
 export function partTwo(input: string[]): number | string {
-  return findComplexity(25, input)
+  const distances = findAccessiblePlots(
+    Math.floor(input[0].length / 2),
+    Math.floor(input.length / 2),
+    input,
+    input.length
+  )
+  return calculateAccessible(input, distances, 26501365)
+}
+
+function calculateAccessible(
+  input: string[],
+  distances: Map<string, number>,
+  steps: number
+): number {
+  const oddCorners = distances
+    .values()
+    .reduce(
+      (total, current) =>
+        current % 2 == 1 && current > 65 ? total + 1 : total,
+      0
+    )
+  const evenCorners = distances
+    .values()
+    .reduce(
+      (total, current) =>
+        current % 2 == 0 && current > 65 ? total + 1 : total,
+      0
+    )
+
+  const even = distances
+    .values()
+    .reduce((total, current) => (current % 2 == 0 ? total + 1 : total), 0)
+  const odd = distances
+    .values()
+    .reduce((total, current) => (current % 2 == 1 ? total + 1 : total), 0)
+
+  const tesselations = (steps - Math.floor(input.length / 2)) / input.length
+
+  const totalTiles =
+    (tesselations + 1) ** 2 * odd +
+    tesselations ** 2 * even -
+    (tesselations + 1) * oddCorners +
+    tesselations * evenCorners
+
+  return totalTiles
 }
