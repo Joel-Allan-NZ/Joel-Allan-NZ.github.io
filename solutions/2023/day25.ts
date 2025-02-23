8f4b311b4e7d90d41f5d923799893f13da5b6861
Joel-Allan-NZ <joelcallan@gmail.com>
2025-02-23 23:05:56 +1300

    feat: added 2023 d25

--- solutions/2023/day25.ts
+++ solutions/2023/day25.ts
@@ -1,34 +1,74 @@
 export function partOne(input: string[]): number | string {
-  const { keys, locks } = parse(input)
+  const graph = new Map<string, Map<string, number>>()
+  for (let line of input) {
+    const split = line.split(' ')
+    const a = split[0].slice(0, 3)
 
-  return keys.reduce(
-    (total, key) => total + locks.filter((lock) => isFit(key, lock, 5)).length,
-    0
-  )
-}
+    if (!graph.has(a)) graph.set(a, new Map<string, number>())
 
-function parse(input: string[]): { keys: number[][]; locks: number[][] } {
-  const DIAGRAMHEIGHT = 7
-  const KEYSIZE = 5
-  const keys: number[][] = []
-  const locks: number[][] = []
+    for (let add of split.slice(1)) {
+      if (!graph.has(add)) graph.set(add, new Map<string, number>())
 
-  for (let i = 0; i < input.length; i += DIAGRAMHEIGHT + 1) {
-    const lockOrKey: number[] = Array(5).fill(-1)
-    for (let j = 0; j < DIAGRAMHEIGHT; j++) {
-      for (let k = 0; k < KEYSIZE; k++) {
-        if (input[i + j][k] == '#') lockOrKey[k]++
-      }
+      graph.get(a)!.set(add, 1)
+      graph.get(add)!.set(a, 1)
     }
-    if (input[i][0] != '#') keys.push(lockOrKey)
-    else locks.push(lockOrKey)
   }
 
-  return { keys, locks }
+  while (true) {
+    const copy = new Map<string, Map<string, number>>()
+    graph.keys().forEach((key) => {
+      copy.set(key, new Map<string, number>())
+      graph
+        .get(key)!
+        .entries()
+        .forEach((v) => copy.get(key)!.set(v[0], v[1]))
+    })
+    const count = karger(copy)
+    const entries = copy.entries().toArray()
+
+    if (entries[0][1].get(entries[1][0]) == 3) {
+      return count.get(entries[0][0])! * count.get(entries[1][0])!
+    }
+  }
 }
 
-function isFit(key: number[], lock: number[], keySize: number): boolean {
-  return key.every((value, index) => lock[index] + value <= keySize)
+function karger(graph: Map<string, Map<string, number>>): Map<string, number> {
+  const count = new Map<string, number>()
+  graph.keys().forEach((key) => count.set(key, 1))
+
+  while (graph.size > 2) {
+    const edge = getRandomEdge(graph)
+    merge(graph, edge)
+    const newCount = count.get(edge[0])! + count.get(edge[1])!
+    count.set(edge[0], newCount)
+  }
+  return count
+}
+
+function getRandomEdge(graph: Map<string, Map<string, number>>): string[] {
+  const u = graph.keys().toArray()[Math.floor(Math.random() * graph.size)]
+  const vSize = graph.get(u)!.size
+  const v = graph.get(u)!.keys().toArray()[Math.floor(Math.random() * vSize)]
+
+  return [u, v]
+}
+
+function merge(graph: Map<string, Map<string, number>>, edge: string[]): void {
+  const vEdges = graph.get(edge[1])!.entries().toArray()
+
+  for (let e of vEdges) {
+    if (e[0] != edge[0]) {
+      let uDistance = graph.get(edge[0])?.get(e[0])
+      uDistance ??= 0
+      graph.get(edge[0])?.set(e[0], e[1] + uDistance)
+
+      graph.get(e[0])?.set(edge[0], graph.get(edge[0])!.get(e[0])!)
+    }
+    graph.get(e[0])?.delete(edge[1])
+  }
+
+  graph.get(edge[0])?.delete(edge[1])
+  graph.delete(edge[1])
 }
 
 export function partTwo(input: string[]): number | string {
