c18f0c87c8a3bceba0b92ca2488754238397024b
Joel-Allan-NZ <joelcallan@gmail.com>
2025-02-23 22:12:31 +1300

    feat: added 2023 d24

--- solutions/2023/day24.ts
+++ solutions/2023/day24.ts
@@ -1,164 +1,122 @@
 export function partOne(input: string[]): number | string {
-  const { wires, gatesByWire } = parse(input)
-  const binaryString = runSystem(wires, gatesByWire)
-  return parseInt(binaryString, 2)
-}
+  const hail = parse(input)
+  const MIN = 200000000000000
+  const MAX = 400000000000000
 
-interface Gate {
-  a: string
-  b: string
-  operation: string
-  output: string
-}
-
-function parse(input: string[]): {
-  wires: Map<string, boolean | undefined>
-  gatesByWire: Map<string, Gate[]>
-} {
-  const wires = new Map<string, boolean | undefined>()
-  const gatesByWire = new Map<string, Gate[]>()
-  let gates = false
-
-  input.forEach((line) => {
-    if (!line || line.length == 0) {
-      gates = true
-    } else {
-      if (gates) {
-        const split = line.split(/ /)
-        const gate: Gate = {
-          a: split[0],
-          b: split[2],
-          operation: split[1],
-          output: split[4],
-        }
-        if (!gatesByWire.has(gate.a)) gatesByWire.set(gate.a, [gate])
-        else gatesByWire.get(gate.a)?.push(gate)
-        if (!gatesByWire.has(gate.b)) gatesByWire.set(gate.b, [gate])
-        else gatesByWire.get(gate.b)?.push(gate)
-
-        if (!wires.has(gate.a)) wires.set(gate.a, undefined)
-        if (!wires.has(gate.b)) wires.set(gate.b, undefined)
-        if (!wires.has(gate.output)) wires.set(gate.output, undefined)
-      } else {
-        const split = line.split(/: /)
-        wires.set(split[0], split[1] === '1')
+  let count = 0
+  for (let i = 0; i < hail.length; i++) {
+    for (let j = i + 1; j < hail.length; j++) {
+      const inter = intercept(hail[i], hail[j])
+      if (isValidIntercept(hail[i], hail[j], inter, MIN, MAX)) {
+        count++
       }
-    }
-  })
-  return { wires, gatesByWire }
-}
-
-function runSystem(
-  wires: Map<string, boolean | undefined>,
-  gates: Map<string, Gate[]>
-): string {
-  let gatesToProcess = [...gates.values().toArray()].flatMap((x) => x)
-  let gatesToProcessNext: Gate[] = []
-
-  while (gatesToProcess.length > 0) {
-    const currentGate = gatesToProcess.shift()!
-    const gateResult = resolveGate(currentGate, wires)
-    if (wires.get(currentGate.output) != gateResult) {
-      wires.set(currentGate.output, gateResult)
-      if (gates.has(currentGate.output)) {
-        gatesToProcessNext = [
-          ...gatesToProcessNext,
-          ...gates.get(currentGate.output)!,
-        ]
-      }
-    }
-    if (gatesToProcess.length == 0) {
-      gatesToProcess = gatesToProcessNext
-      gatesToProcessNext = []
     }
   }
-
-  return wires
-    .keys()
-    .filter((x) => x.startsWith('z'))
-    .toArray()
-    .sort((x, y) => (y > x ? 1 : y == x ? 0 : -1))
-    .reduce(
-      (total, current) => (wires.get(current) ? total + '1' : total + '0'),
-      ''
-    )
+  return count
 }
 
-function resolveGate(
-  gate: Gate,
-  wires: Map<string, boolean | undefined>
-): boolean | undefined {
-  const a = wires.get(gate.a)
-  const b = wires.get(gate.b)
+function parse(input: string[]): number[][][] {
+  const hail: number[][][] = []
+  input.forEach((line) => {
+    const split = line.split('@')
+    hail.push(split.map((x) => x.split(', ').map((x) => parseInt(x))))
+  })
+  return hail
+}
 
-  if (a === undefined || b === undefined) return undefined
-  if (gate.operation == 'AND') return a && b
-  if (gate.operation == 'OR') return a || b
-  if (gate.operation == 'XOR') return a != b
+function intercept(a: number[][], b: number[][]): number[] {
+  if (a[1][1] * b[1][0] - a[0][0] * b[1][1] == 0) return [-1, -1]
+
+  const numerator =
+    a[0][0] * b[1][1] -
+    a[0][1] * b[1][0] -
+    b[0][0] * b[1][1] +
+    b[0][1] * b[1][0]
+  const denominator = a[1][1] * b[1][0] - a[1][0] * b[1][1]
+
+  const lambda = numerator / denominator
+
+  return [a[0][0] + lambda * a[1][0], a[0][1] + lambda * a[1][1]]
+}
+
+function isValidIntercept(
+  a: number[][],
+  b: number[][],
+  intercept: number[],
+  min: number,
+  max: number
+): boolean {
+  if (
+    intercept.some(
+      (i, index) =>
+        (i < a[0][index] && a[1][index] > 0) ||
+        (i < b[0][index] && b[1][index] > 0)
+    )
+  )
+    return false
+  if (
+    intercept.some(
+      (i, index) =>
+        (i > a[0][index] && a[1][index] < 0) ||
+        (i > b[0][index] && b[1][index] < 0)
+    )
+  )
+    return false
+
+  return intercept.every((i) => i >= min && i <= max)
 }
 
 export function partTwo(input: string[]): number | string {
-  const { wires, gatesByWire } = parse(input)
-  const zBits =
-    'z' +
-    wires
-      .keys()
-      .reduce((sum, wire) => (wire.startsWith('z') ? sum + 1 : sum), -1)
-  const gates = [...new Set([...gatesByWire.values().flatMap((x) => x)])]
-  const incorrectGates = gates.filter(
-    (gate) => !isValidGate(gatesByWire, gate, zBits)
-  )
+  const hail = parse(input)
+  const velocityVector = findIntersectVelocity(hail)
 
-  return incorrectGates
-    .map((x) => x.output)
-    .sort()
-    .join(',')
+  const a = hail[0]
+  const ab = [a, hail[1]]
+
+  const m = ab.map(
+    (x) => (x[1][1] - velocityVector[1]) / (x[1][0] - velocityVector[0])
+  )
+  const c = ab.map((x, index) => x[0][1] - m[index] * x[0][0])
+
+  const x = Math.floor((c[1] - c[0]) / (m[0] - m[1]))
+  const y = Math.floor(m[0] * x + c[0])
+  const z =
+    a[0][2] +
+    ((a[1][2] - velocityVector[2]) * (x - a[0][0])) /
+      (a[1][0] - velocityVector[0])
+
+  return x + y + z
 }
 
-function isValidGate(
-  gatesByWire: Map<string, Gate[]>,
-  gate: Gate,
-  zBits: string
-): boolean {
-  if (isFullAdderStart(gate)) {
-    if (gatesByWire.has(gate.output)) {
-      const childGates = gatesByWire.get(gate.output)!
-      if (
-        gate.operation == 'AND' &&
-        childGates.some((x) => x.operation == 'AND')
-      )
-        return false
-      if (
-        gate.operation == 'XOR' &&
-        childGates.some((x) => x.operation == 'OR')
-      )
-        return false
+function findIntersectVelocity(hail: number[][][]): number[] {
+  const sets = [new Set<number>(), new Set<number>(), new Set<number>()]
+
+  for (let i = 0; i < hail.length; i++) {
+    for (let j = i + 1; j < hail.length; j++) {
+      for (let k = 0; k < 3; k++) {
+        const res = findPotentialIntersectVelocity(hail[i], hail[j], k, 2000)
+        if (res.size > 0) {
+          sets[k] = sets[k].size > 0 ? sets[k].intersection(res) : res
+        }
+      }
     }
   }
-  if (isFullAdderEnd(gate, zBits) && gate.operation != 'XOR') return false
-  if (isAdderMiddle(gate) && gate.operation == 'XOR') return false
-  return true
+  return sets.map((x) => x.values().toArray()[0])
 }
 
-function isAdderMiddle(gate: Gate): boolean {
-  return (
-    !gate.a.startsWith('x') &&
-    !gate.a.startsWith('y') &&
-    !gate.b.startsWith('x') &&
-    !gate.b.startsWith('y') &&
-    !gate.output.startsWith('z')
-  )
-}
-
-function isFullAdderEnd(gate: Gate, zBits: string): boolean {
-  return gate.output.startsWith('z') && gate.output != zBits
-}
-
-function isFullAdderStart(gate: Gate) {
-  return (
-    (gate.a.startsWith('x') || gate.a.startsWith('y')) &&
-    (gate.b.startsWith('x') || gate.b.startsWith('y')) &&
-    !gate.a.endsWith('00') &&
-    !gate.b.endsWith('00')
-  )
+function findPotentialIntersectVelocity(
+  a: number[][],
+  b: number[][],
+  vectorDimension: number,
+  speedRange: number
+): Set<number> {
+  const potential = new Set<number>()
+  if (a[1][vectorDimension] == b[1][vectorDimension]) {
+    const diff = b[0][vectorDimension] - a[0][vectorDimension]
+    for (let v = -1 * speedRange; v <= speedRange; v++) {
+      const c = v - a[1][vectorDimension]
+      if (c != 0 && diff != 0 && ((diff % c) + c) % c == 0) potential.add(v)
+    }
+  }
+  return potential
 }
